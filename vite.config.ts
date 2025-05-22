9e2b7111487d8cfc4100896735e54a0aa2d97d1e
Cory Twitty <twittyc@gmail.com>
2025-05-22 11:29:47 -0600

    update github acitons

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/', // This will be your custom domain
   server: {
     host: "::",
     port: 8080,
